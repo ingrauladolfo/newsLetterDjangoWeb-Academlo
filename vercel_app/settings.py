034b477171e6dc939e8f1725e6661bf5d0ef0bb8
Ing. Raúl Adolfo Torres Vargas <ingrauladolfo.torresvargas@gmail.com>
2021-11-26 11:10:22 -0600

    Update .gitignore and all settings.py

--- vercel_app/settings.py
+++ vercel_app/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = '+t*-ks%h^rho+5nyi@@ai$wv6!0%gpesn^469%104(u(5$_6a+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','.vercel.app']
-
+ALLOWED_HOSTS = ['.vercel.app']
 
 # Application definition
 
